ff8131b3606ca9069bafcec07d57ae999e168e2b
BradJKim <bradjkim03@gmail.com>
2023-08-02 18:28:43 -0700

    functional grid and reset button, nonfunctional scroll bars

--- javascript.js
+++ javascript.js
@@ -1,26 +1,27 @@
 const container = document.getElementById("container");
 
-fillGrid(16);
+container.style.cssText = 'width: 360px; height: 360px; margin: auto; display: grid; grid-template-columns: repeat(8, auto); grid-template-rows: repeat(8, auto)'
+
+fillGrid(8);
 
 
 
 function fillGrid(number){
-    let divsRow = null;
-    let divsCol = null;
+    container.style.cssText = `width: 360px; height: 360px; margin: auto; display: grid; grid-template-columns: repeat(${number}, auto); grid-template-rows: repeat(${number}, auto)`
 
-    container.style.cssText = "display: flex; justify-content: center;";
+    for(let i=0; i<number*number; i++){
+        const div1 = document.createElement('div');
+        div1.style.cssText = "height: auto; border: 1px solid black;";
+        div1.addEventListener('mouseover', () => {        
+            div1.style.cssText += "background: black";
+        });
+        container.appendChild(div1);
+    }  
+}
 
-    for(let i=0; i<number;i++){
-        divsRow = document.createElement('div');
-        divsRow.setAttribute("id", "divsRow");
-        divsRow.style.cssText = "width: 16px; height: 16px;";
-        
-        for(let i=0; i<number;i++){
-            divsCol = document.createElement('div');
-            divsCol.style.cssText = "width: 16px; height: 16px; border: 1px solid black;";
-            divsRow.appendChild(divsCol);
-        }
-
-        container.appendChild(divsRow);
-    }
+function resetGrid(){
+    let containers = container.querySelectorAll('div')
+    containers.forEach(div =>{
+        div.style.cssText = "height: auto; border: 1px solid black; background: none;";
+    })
 }
