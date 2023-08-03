2d17d3cde3342d997083e9d59604d714a13b12d2
BradJKim <bradjkim03@gmail.com>
2023-08-03 14:51:21 -0700

    working grid with adjustable grid length bar

--- javascript.js
+++ javascript.js
@@ -1,17 +1,29 @@
 const container = document.getElementById("container");
+const gridLengthText = document.querySelector('#gridLengthText');
+const gridBtn = document.querySelector('#gridBtn');
 
-container.style.cssText = 'width: 360px; height: 360px; margin: auto; display: grid; grid-template-columns: repeat(8, auto); grid-template-rows: repeat(8, auto)'
+container.style.cssText = 'width: 360px; height: 360px; margin: auto; display: grid; border: 1px solid blue;'
 
-fillGrid(8);
+fillGrid(16);
+gridLengthText.textContent = `16 x 16`
 
+gridBtn.addEventListener('change', () => {
+    let roundedValue = Math.floor(gridBtn.value);
+    resetGrid();
+    fillGrid(roundedValue);
+    gridLengthText.textContent = `${roundedValue} x ${roundedValue}`
+});
 
+function changeColor(){
+    
+}
 
 function fillGrid(number){
-    container.style.cssText = `width: 360px; height: 360px; margin: auto; display: grid; grid-template-columns: repeat(${number}, auto); grid-template-rows: repeat(${number}, auto)`
+    container.style.gridTemplateColumns = `repeat(${number}, 1fr)`; container.style.gridTemplateRows = `repeat(${number}, 1fr)`
 
     for(let i=0; i<number*number; i++){
         const div1 = document.createElement('div');
-        div1.style.cssText = "height: auto; border: 1px solid black;";
+        div1.style.cssText = "user-select: none;";
         div1.addEventListener('mouseover', () => {        
             div1.style.cssText += "background: black";
         });
@@ -22,6 +34,6 @@ function fillGrid(number){
 function resetGrid(){
     let containers = container.querySelectorAll('div')
     containers.forEach(div =>{
-        div.style.cssText = "height: auto; border: 1px solid black; background: none;";
+        div.style.cssText = "user-select: none; ";
     })
 }
